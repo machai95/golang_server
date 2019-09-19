a0282711df1922dce3c6d9cc0f48a2c49d31933e
HaiMD <haimd@beetsoft.com.vn>
2019-09-19 16:57:10 +0700

    debug

--- WebServer/database.go
+++ WebServer/database.go
@@ -1,31 +1,39 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "strconv"
+	"database/sql"
+	"fmt"
+	"strconv"
 
-    _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	database, _ := 
+	fmt.Printf("Creat database")
+	database, _ :=
 		sql.Open("sqlite3", "./users.db")
-	statement, _ := 
+	fmt.Println("---> done")
+	fmt.Printf("Create table user")
+	statement, _ :=
 		database.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password NUMBERIC,timecreat DATETIME CURRENT_TIMESTAMP)")
-    statement.Exec()
-	statement, _ = 
+	statement.Exec()
+	fmt.Println("---> done")
+	fmt.Printf("Insert data into table user")
+	statement, _ =
 		database.Prepare("INSERT INTO users (username, password) VALUES (?, ?,?,?)")
-     ("Haimd", "123456","datetime()")
-	rows, _ := 
+	statement.Exec("Haimd", "123456", "datetime()")
+	fmt.Println("---> done")
+	fmt.Printf("Query data into table user")
+	rows, _ :=
 		database.Query("SELECT id, username, password, timecreat FROM users")
-    var id int
-    var user string
-    var pass string
+	fmt.Println("---> done")
+	var id int
+	var user string
+	var pass string
 
-    for rows.Next() {
-        rows.Scan(&id, &user, &pass)
-        fmt.Println(strconv.Itoa(id) + ": " + user + " " + pass)
-    }
+	for rows.Next() {
+		rows.Scan(&id, &user, &pass)
+		fmt.Println(strconv.Itoa(id) + ": " + user + " " + pass)
+	}
 
 }
